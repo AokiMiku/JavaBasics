759da8bc898d8e66d1f6fcbc25951079c608b0b5
Melissa Miku Menzel <nekomeli@outlook.com>
2021-02-19 09:04:07 +0100

    practicing with recursion

---
+++ src/basics/RecursionTest.java
@@ -0,0 +1,21 @@
+package basics;
+
+public class RecursionTest 
+{
+	public static void main(String[] args) 
+	{
+		int zahl = 7;
+		long fakultaet = berechneFakultaet(zahl);
+		System.out.println(zahl + "! = " + fakultaet);
+	}
+
+	private static long berechneFakultaet(int zahl) 
+	{
+		if (zahl == 1)
+		{
+			return zahl;
+		}
+		
+		return zahl *= berechneFakultaet(zahl - 1);
+	}
+}
