586cdbefeeaffb706c56fc718913551fbe4079bf
Melissa Miku Menzel <nekomeli@outlook.com>
2021-02-24 13:03:48 +0100

    added styles to the interest calculator; prevented the input of non-numerical keys; added focus changes on enter-press

--- src/mvc/controller/ZinsrechnerController.java
+++ src/mvc/controller/ZinsrechnerController.java
@@ -7,7 +7,7 @@ import javax.swing.JOptionPane;
 import mvc.model.*;
 import mvc.view.*;
 
-public class ZinsrechnerController extends WindowAdapter implements ActionListener
+public class ZinsrechnerController extends WindowAdapter implements ActionListener, KeyListener
 {
 	IZinsrechnerModel model;
 	ZinsrechnerView view;
@@ -20,7 +20,7 @@ public class ZinsrechnerController extends WindowAdapter implements ActionListen
 	
 	public double berechne(int modus)
 	{
-		int laufzeit = 0;
+		double laufzeit = 0;
 		double endkapital = 0;
 		double startkapital = Double.parseDouble(this.view.getTxtStartkapital().getText().replace(",", "."));
 		float zinsPA = Float.parseFloat(this.view.getTxtZinsPA().getText().replace(",", "."));
@@ -28,12 +28,19 @@ public class ZinsrechnerController extends WindowAdapter implements ActionListen
 		{
 			case 0:
 				this.view.getTxtKapitalProJahr().setText("");
-				laufzeit = Integer.parseInt(this.view.getTxtLaufzeit().getText());
+				laufzeit = Double.parseDouble(this.view.getTxtLaufzeit().getText().replace(",", "."));
 				for (int i = 1; i <= laufzeit; i++)
 				{
-					this.view.getTxtKapitalProJahr().setText(String.format("%s %n%20s %,40.2f", this.view.getTxtKapitalProJahr().getText(), i, this.model.berechneEndkapital(startkapital, i, zinsPA)));
+					if (i == 1)
+					{
+						this.view.getTxtKapitalProJahr().setText(String.format("%10s| %,30.2f",  i, this.model.berechneEndkapital(startkapital, i, zinsPA)));
+					}
+					else
+					{
+						this.view.getTxtKapitalProJahr().setText(String.format("%s %n%10s| %,30.2f", this.view.getTxtKapitalProJahr().getText(), i, this.model.berechneEndkapital(startkapital, i, zinsPA)));
+					}
 				}
-				return this.model.berechneEndkapital(startkapital, laufzeit, zinsPA);
+				return this.model.berechneEndkapital(startkapital, (int)laufzeit, zinsPA);
 			case 1:
 				endkapital = Double.parseDouble(this.view.getTxtEndkapital().getText().replace(",", "."));
 				return this.model.berechneLaufzeit(startkapital, zinsPA, endkapital);
@@ -83,6 +90,8 @@ public class ZinsrechnerController extends WindowAdapter implements ActionListen
 
 	private void clearView() 
 	{
+		this.view.getTxtStartkapital().requestFocusInWindow();
+		
 		this.view.getTxtEndkapital().setText("");
 		this.view.getTxtLaufzeit().setText("");
 		this.view.getTxtStartkapital().setText("");
@@ -106,6 +115,7 @@ public class ZinsrechnerController extends WindowAdapter implements ActionListen
 			else if (this.getModus() == 1)
 			{
 				this.view.getTxtLaufzeit().setText(String.format("%,.2f", this.berechne(1)));
+				this.berechne(0);
 			}
 		}
 		else if (e.getSource() == this.view.getRbEndkapital() || e.getSource() == this.view.getRbLaufzeit())
@@ -122,5 +132,63 @@ public class ZinsrechnerController extends WindowAdapter implements ActionListen
 	public void windowClosing(WindowEvent e) 
 	{
 		this.beenden();
+	}
+	
+	@Override
+	public void windowOpened(WindowEvent e) 
+	{
+		this.view.getTxtStartkapital().requestFocusInWindow();
+	}
+
+	@Override
+	public void keyPressed(KeyEvent e) 
+	{
+		if (e.getKeyCode() == 10)
+		{
+			if (e.getSource() == this.view.getTxtStartkapital())
+			{
+				this.view.getTxtZinsPA().requestFocusInWindow();
+			}
+			else if (e.getSource() == this.view.getTxtZinsPA())
+			{
+				if (this.getModus() == 0)
+				{
+					this.view.getTxtLaufzeit().requestFocusInWindow();
+				}
+				else if (this.getModus() == 1)
+				{
+					this.view.getTxtEndkapital().requestFocusInWindow();
+				}
+			}
+			else if (e.getSource() == this.view.getTxtLaufzeit() || e.getSource() == this.view.getTxtEndkapital())
+			{
+				this.view.getBtnBerechne().doClick();
+			}
+			System.out.println("-----");
+		}
+		else if (e.getKeyCode() == 27)
+		{
+			this.beenden();
+		}
+		else
+		{
+			System.out.println(String.format("%s (%s): %s", e.getKeyChar(), (int)e.getKeyChar(), e.getKeyCode()));
+		}
+	}
+
+	@Override
+	public void keyReleased(KeyEvent e) 
+	{
+	
+	}
+
+	@Override
+	public void keyTyped(KeyEvent e) 
+	{
+		if ((e.getKeyChar() > '9' || e.getKeyChar() < '0') && e.getKeyChar() != ',' && e.getKeyChar() != '.' && e.getKeyCode() != 0)
+		{
+			System.out.println("Falsch! Code: " + e.getKeyCode() + " Char: " + e.getKeyChar());
+			throw new IllegalArgumentException();
+		}	
 	}
 }
--- src/mvc/view/ZinsrechnerView.java
+++ src/mvc/view/ZinsrechnerView.java
@@ -1,36 +1,57 @@
 package mvc.view;
 
-import java.awt.Dimension;
-import java.awt.GridBagConstraints;
-import java.awt.GridBagLayout;
+import java.awt.Color;
+import java.awt.FlowLayout;
+import java.awt.Font;
+import java.awt.GridLayout;
 import javax.swing.*;
+import javax.swing.border.*;
+
 import mvc.controller.ZinsrechnerController;
 
 public class ZinsrechnerView extends JFrame
 {
-	JLabel lblStartkapital;
-	JLabel lblEndkapital;
-	JLabel lblZinsPA;
-	JLabel lblLaufzeit;
+	private JLabel lblStartkapital;
+	private JLabel lblEndkapital;
+	private JLabel lblZinsPA;
+	private JLabel lblLaufzeit;
 	
-	JTextField txtStartkapital;
+	private JTextField txtStartkapital;
 
-	JTextField txtEndkapital;
-	JTextField txtZinsPA;
-	JTextField txtLaufzeit;
+	private JTextField txtEndkapital;
+	private JTextField txtZinsPA;
+	private JTextField txtLaufzeit;
 	
-	ButtonGroup bgModus;
-	JRadioButton rbEndkapital;
-	JRadioButton rbLaufzeit;
+	private ButtonGroup bgModus;
+	private JRadioButton rbEndkapital;
+	private JRadioButton rbLaufzeit;
 	
-	JButton btnBerechne;
-	JButton btnLoeschen;
-	JButton btnBeenden;
+	private JButton btnBerechne;
+	private JButton btnLoeschen;
+	private JButton btnBeenden;
 	
-	JTextArea txtKapitalProJahr;
+	private JTextArea txtKapitalProJahr;
 	
-	JPanel pnlInhalt;
+	private JPanel pnlView;
+	private JPanel pnlInhalt;
+	private JPanel pnlButtons;
+	private JPanel pnlText;
+	private JScrollPane spnText;
 	
+	private Color backgroundColor = new Color(20, 168, 121);
+	private Color fontColor = new Color(255, 255, 255);
+
+	private Color borderColor = new Color(0, 0, 0);
+//	private int buttonBorderThickness = 1;
+	private Border buttonBorder = BorderFactory.createBevelBorder(BevelBorder.RAISED, this.borderColor.brighter(), this.borderColor.darker());
+//	private Border buttonBorder = BorderFactory.createCompoundBorder(
+//									BorderFactory.createBevelBorder(BevelBorder.RAISED, this.borderColor.brighter(), this.borderColor.darker()), 
+//									BorderFactory.createEtchedBorder(EtchedBorder.LOWERED));
+	private java.awt.Cursor buttonCursor = new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR);
+	
+	private Font textFont = new Font("Consolas", Font.PLAIN, 14);
+	private Font labelFont = new Font("Arial", Font.PLAIN, 14);
+
 	private ZinsrechnerController controller;
 	
 	public JTextField getTxtStartkapital() 
@@ -91,13 +112,14 @@ public class ZinsrechnerView extends JFrame
 	public ZinsrechnerView() 
 	{
 		super("Zinsrechner");
-		this.setSize(400, 600);
-		this.setMinimumSize(new Dimension(400, 600));
+		this.setSize(500, 600);
+		this.setResizable(false);
 		this.setLocationRelativeTo(null);
 		this.setDefaultCloseOperation(DO_NOTHING_ON_CLOSE);
 		this.controller = new ZinsrechnerController(this);
 		this.initComponents();
 		this.addComponents();
+		this.styleComponents();
 		this.setVisible(true);
 		this.addWindowListener(this.controller);
 	}
@@ -111,106 +133,152 @@ public class ZinsrechnerView extends JFrame
 		
 		this.txtEndkapital = new JTextField(10);
 		this.txtEndkapital.setEditable(false);
+		this.txtEndkapital.addKeyListener(this.controller);
+		
 		this.txtLaufzeit = new JTextField(10);
+		this.txtLaufzeit.addKeyListener(this.controller);
+		
 		this.txtStartkapital = new JTextField(10);
+		this.txtStartkapital.addKeyListener(this.controller);
+		
 		this.txtZinsPA = new JTextField(10);
+		this.txtZinsPA.addKeyListener(this.controller);
 		
 		this.bgModus = new ButtonGroup();
+		
 		this.rbEndkapital = new JRadioButton("Endkapital");
 		this.rbEndkapital.setSelected(true);
 		this.rbEndkapital.addActionListener(this.controller);
+		
 		this.rbLaufzeit = new JRadioButton("Laufzeit");
 		this.rbLaufzeit.addActionListener(this.controller);
 		
 		this.btnBeenden = new JButton("Beenden");
 		this.btnBeenden.addActionListener(this.controller);
+		
 		this.btnBerechne = new JButton("Berechne");
 		this.btnBerechne.addActionListener(this.controller);
+		
 		this.btnLoeschen = new JButton("Löschen");
 		this.btnLoeschen.addActionListener(controller);
 		
-		this.txtKapitalProJahr = new JTextArea();
+		this.txtKapitalProJahr = new JTextArea(15, 40);
 		this.txtKapitalProJahr.setEditable(false);
 
+		this.pnlView = new JPanel();
 		this.pnlInhalt = new JPanel();
+		this.pnlButtons = new JPanel();
+		this.pnlText = new JPanel();
+		this.spnText = new JScrollPane(this.txtKapitalProJahr);
+		this.spnText.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_AS_NEEDED);
+		this.spnText.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
 	}
 	
 	public void addComponents()
 	{
-		this.pnlInhalt.setLayout(new GridBagLayout());
-		GridBagConstraints c;
-		
-		c = new GridBagConstraints();
-		c.fill = GridBagConstraints.NONE;
-		c.ipadx = 20;
-		c.ipady = 20;
-		c.weightx = 0.5;
-		c.weighty = 0.2;
-		c.gridwidth = 2;
-		
-		c.gridx = 0;
-		c.gridy = 0;
-		this.pnlInhalt.add(this.lblStartkapital, c);
-		
-		c.gridx = 2;
-		c.gridy = 0;		
-		this.pnlInhalt.add(this.txtStartkapital, c);
-		
-		c.gridx = 0;
-		c.gridy = 2;		
-		this.pnlInhalt.add(this.lblZinsPA, c);
-		
-		c.gridx = 2;
-		c.gridy = 2;		
-		this.pnlInhalt.add(this.txtZinsPA, c);
-		
-		c.gridx = 0;
-		c.gridy = 1;		
-		this.pnlInhalt.add(this.lblLaufzeit, c);
-		//
-		c.gridx = 2;
-		c.gridy = 1;		
-		this.pnlInhalt.add(this.txtLaufzeit, c);
-		
-		c.gridx = 0;
-		c.gridy = 3;		
-		this.pnlInhalt.add(this.lblEndkapital, c);
-		
-		c.gridx = 2;
-		c.gridy = 3;		
-		this.pnlInhalt.add(this.txtEndkapital, c);
-		
-		c.gridx = 0;
-		c.gridy = 4;		
-		this.pnlInhalt.add(this.rbEndkapital, c);
+		this.pnlView.setLayout(new FlowLayout(FlowLayout.CENTER, 10000, 15));
+		this.pnlInhalt.setLayout(new GridLayout(5, 2, 20, 20));
+		this.pnlButtons.setLayout(new GridLayout(1, 3, 5, 5));
+		this.pnlText.setLayout(new GridLayout(1, 1, 10, 10));
+
+		this.pnlInhalt.add(this.rbEndkapital);
 		this.bgModus.add(this.rbEndkapital);
-		
-		c.gridx = 2;
-		c.gridy = 4;		
-		this.pnlInhalt.add(this.rbLaufzeit, c);
+		this.pnlInhalt.add(this.rbLaufzeit);
 		this.bgModus.add(this.rbLaufzeit);
 		
-		c.gridwidth = 1;
-		c.gridx = 0;
-		c.gridy = 5;
-		this.pnlInhalt.add(this.btnBerechne, c);
+		this.pnlInhalt.add(this.lblStartkapital);
+		this.pnlInhalt.add(this.txtStartkapital);
 		
-		c.gridx = 1;
-		c.gridy = 5;		
-		this.pnlInhalt.add(this.btnLoeschen, c);
+		this.pnlInhalt.add(this.lblZinsPA);
+		this.pnlInhalt.add(this.txtZinsPA);
 		
-		c.gridx = 2;
-		c.gridy = 5;		
-		this.pnlInhalt.add(this.btnBeenden, c);
+		this.pnlInhalt.add(this.lblLaufzeit);
+		this.pnlInhalt.add(this.txtLaufzeit);
 		
-		c.gridwidth = 3;
-		c.gridheight = 3;
-		c.fill = GridBagConstraints.BOTH;
-		c.weighty = 1;
-		c.gridx = 0;
-		c.gridy = 7;
-		this.pnlInhalt.add(this.txtKapitalProJahr, c);
+		this.pnlInhalt.add(this.lblEndkapital);
+		this.pnlInhalt.add(this.txtEndkapital);
+
+		this.pnlButtons.add(this.btnBerechne);
+		this.pnlButtons.add(this.btnLoeschen);
+		this.pnlButtons.add(this.btnBeenden);
 		
-		this.setContentPane(this.pnlInhalt);
+		this.pnlText.add(this.spnText);
+		
+		this.pnlView.add(this.pnlInhalt);
+		this.pnlView.add(this.pnlButtons);
+		this.pnlView.add(this.pnlText);
+		this.setContentPane(this.pnlView);
+	}
+	
+	private void styleComponents()
+	{
+		// Panels ==================================================================
+		this.pnlView.setBackground(this.backgroundColor);
+		this.pnlView.setForeground(this.fontColor);
+		
+		this.pnlInhalt.setBackground(this.backgroundColor);
+		this.pnlInhalt.setForeground(this.fontColor);
+		
+		this.pnlButtons.setBackground(this.backgroundColor);
+		this.pnlButtons.setForeground(this.fontColor);
+		
+		this.pnlText.setBackground(this.backgroundColor);
+		this.pnlText.setForeground(this.fontColor);
+
+		this.spnText.setBorder(null);
+		
+		// RadioButtons ==================================================================
+		this.rbEndkapital.setBackground(this.backgroundColor);
+		this.rbEndkapital.setForeground(this.fontColor);
+		this.rbEndkapital.setFont(this.labelFont);
+
+		this.rbLaufzeit.setBackground(this.backgroundColor);
+		this.rbLaufzeit.setForeground(this.fontColor);
+		this.rbLaufzeit.setFont(this.labelFont);
+		
+		// Buttons ==================================================================
+		this.btnBeenden.setBackground(this.backgroundColor);
+		this.btnBeenden.setForeground(this.fontColor);
+		this.btnBeenden.setBorder(this.buttonBorder);
+		this.btnBeenden.setCursor(buttonCursor);
+		this.btnBeenden.setFont(this.labelFont);
+		
+		this.btnBerechne.setBackground(this.backgroundColor);
+		this.btnBerechne.setForeground(this.fontColor);
+		this.btnBerechne.setBorder(this.buttonBorder);
+		this.btnBerechne.setCursor(buttonCursor);
+		this.btnBerechne.setFont(this.labelFont);
+		
+		this.btnLoeschen.setBackground(this.backgroundColor);
+		this.btnLoeschen.setForeground(this.fontColor);
+		this.btnLoeschen.setBorder(this.buttonBorder);
+		this.btnLoeschen.setCursor(buttonCursor);
+		this.btnLoeschen.setFont(this.labelFont);
+		
+		// Labels ==================================================================
+		this.lblEndkapital.setForeground(this.fontColor);
+		this.lblEndkapital.setFont(this.labelFont);
+		
+		this.lblLaufzeit.setForeground(this.fontColor);
+		this.lblLaufzeit.setFont(this.labelFont);
+		
+		this.lblStartkapital.setForeground(this.fontColor);
+		this.lblStartkapital.setFont(this.labelFont);
+		
+		this.lblZinsPA.setForeground(this.fontColor);
+		this.lblZinsPA.setFont(this.labelFont);
+		
+		// Text Components ==================================================================
+		this.txtKapitalProJahr.setFont(this.textFont);
+		this.txtKapitalProJahr.setBackground(this.backgroundColor);
+		this.txtKapitalProJahr.setForeground(this.fontColor);
+		
+		this.txtEndkapital.setFont(this.textFont);
+		
+		this.txtLaufzeit.setFont(this.textFont);
+		
+		this.txtStartkapital.setFont(this.textFont);
+		
+		this.txtZinsPA.setFont(this.textFont);
 	}
 }
